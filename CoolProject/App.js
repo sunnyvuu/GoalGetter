e9ac847cf20d6bb7da1ad6baab98eea76835ac5b
Madeline Yi <sunhunfunbun@gmail.com>
2023-02-15 16:04:40 -0600

    fixed inputContainer + added goalsContainer style

--- CoolProject/App.js
+++ CoolProject/App.js
@@ -8,7 +8,7 @@ export default function App() {
         <TextInput placeholder='Enter your goal here'style={styles.textInput}/>
         <Button title='Add Goal'/>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text> List of Goals...</Text>
       </View>
     </View>
@@ -17,17 +17,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1, 
     padding: 50,
+    paddingHorizontal: 16,
+    
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc"
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8
+  },
+  goalsContainer: {
+    flex: 5
   }
 });
