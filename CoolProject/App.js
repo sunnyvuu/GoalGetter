9b30c3c37c2555f70f4176bf069931a26ad71617
Madeline Yi <sunhunfunbun@gmail.com>
2023-02-15 15:46:53 -0600

    added styling to the TextInput

--- CoolProject/App.js
+++ CoolProject/App.js
@@ -4,8 +4,8 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Enter your goal here'/>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter your goal here'style={styles.textInput}/>
         <Button title='Add Goal'/>
       </View>
       <View>
@@ -19,4 +19,15 @@ const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8
+  }
 });
