7fbad4d436e7fc73f43fc41619bd29c495c306c9
Madeline Yi <sunhunfunbun@gmail.com>
2023-02-18 10:34:42 -0600

    created a completed button with styling

--- CoolProject/components/GoalItem.js
+++ CoolProject/components/GoalItem.js
@@ -2,21 +2,46 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View>
+        <View style={styles.goalContainer}>
+            <View style= {styles.goalItemContainer}>
                 <Text style={styles.goalItem}>{props.text}</Text>
             </View>
-        </Pressable>
+            <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+                <View style={styles.CustomButton}>
+                    <Text>Completed!</Text>
+                </View>
+            </Pressable>
+        </View>
+        
     )
 };
 
 export default GoalItem;
 
 const styles = StyleSheet.create({
+    goalContainer: {
+      flexDirection: "row",  
+      alignContent: "space-between",
+    },
+    goalItemContainer: {
+        flexShrink: 1,
+        flexBasis: "auto",
+        flexGrow: 1,
+    },
     goalItem: {
-        margin: 8,
+        marginTop: 8,
+        marginBottom: 8,
         padding: 8,
         borderRadius: 6,
         backgroundColor: "#f0dadb"
+      },
+      CustomButton: {
+        flexShrink: 0,
+        flexBasis: "auto",
+        flexGrow: 0,
+        backgroundColor: "pink",
+        margin: 8,
+        padding: 8,
+        borderRadius: 6,
       }
 });
